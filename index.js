d83bce39a9f36a20c03b571e776b768db19a1788
Fabien HENON <henon.fabien@softcreations.fr>
2017-03-13 16:53:14 +0100

    Add release command

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const {exec} = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
-function puts(next) {
+function puts(next, canFail) {
   return (error, stdout, stderr) => {
     console.log(stdout);
 
-    if (error) {
+    if (error && !canFail) {
       throw error;
     } else {
       next && next();
@@ -16,9 +16,14 @@ function puts(next) {
   }
 }
 
-function cmd(cmd, next) {
+function cmd(cmd, next, canFail) {
   console.log(cmd);
-  exec(cmd, puts(next));
+  exec(cmd, puts(next, !!canFail));
+}
+
+function getVersionTag() {
+  var json = JSON.parse(fs.readFileSync('elm-package.json', 'utf8'));
+
 }
 
 function bump(args) {
@@ -47,13 +52,35 @@ function build(args) {
   const image = args[0];
   const tag = args.length > 1 && args[1] == 'staging' ? 'staging-build' : 'build';
 
-  const dockerFile = path.resolve(__dirname, 'scripts/Dockerfile.staging.build');
+  const file = args.length > 1 && args[1] == 'staging' ? 'Dockerfile.staging.build' : 'Dockerfile.build';
+  const dockerFile = path.resolve(__dirname, 'scripts/' + file);
 
   cmd(`docker build -f ${dockerFile} -t ${image}:${tag} .`);
 }
 
 function release(args) {
+  if (args.length == 0) {
+    throw 'Image name is needed (without tag)'
+  }
 
+  const image = args[0];
+  const tag = args.length > 1 && args[1] == 'staging' ? 'staging-release' : 'release';
+  const tagBuild = args.length > 1 && args[1] == 'staging' ? 'staging-build' : 'build';
+
+  const cid = `elm_deploy_${Math.random()}`
+
+  cmd(`docker rm -f ${cid}`, () => {
+    cmd(`docker create --name ${cid} ${image}:${tagBuild}`, () => {
+      cmd(`docker cp ${cid}:/opt/app/dist dist`, () => {
+        cmd(`docker rm -f ${cid}`, () => {
+          const file = args.length > 1 && args[1] == 'staging' ? 'Dockerfile.staging.release' : 'Dockerfile.release';
+          const dockerFile = path.resolve(__dirname, 'scripts/' + file);
+
+          cmd(`docker build -f ${dockerFile} -t ${image}:${tag} .`);
+        }, true);
+      });
+    });
+  }, true);
 }
 
 function publish(args) {
